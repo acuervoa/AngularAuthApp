160b24225bb70810fe497c2bc9850c7870aa5afd
andres.cuervo <andres.cuervo@tecnilogica.com>
2018-01-03 10:51:05 +0100

    solve conflicts

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -8,11 +8,8 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavBarComponent {
 
-<<<<<<< HEAD
-  constructor( pubic auth: AuthService) {
-=======
+
   constructor( public auth: AuthService) {
->>>>>>> view google profile with auth0
     auth.handleAuthentication();
   }
 
